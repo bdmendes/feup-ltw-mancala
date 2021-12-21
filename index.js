ec9f696ef88457ef56a256734aec653129cb6413
Bruno Mendes <brod56@sapo.pt>
2021-12-21 21:54:42 +0000

    Add dom board; add vscode recommendations

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import Game from './scripts/game.js';
-import Player from './scripts/player.js';
+import Game from "./scripts/game.js";
+import Player from "./scripts/player.js";
 
-window.onload = function() {
-  console.log('Hello world');
-  const game = new Game(
-      new Player('Jose', 'sirze', '123'), new Player('Bruno', 'brocolo', 345),
-      6);
-  console.log(game.player1.name);
-}
+window.onload = function () {
+    console.log("Hello world");
+    const game = new Game(
+        new Player("Jose", "sirze", "123"),
+        new Player("Bruno", "brocolo", 345)
+    );
+    console.log(game.players[0].name);
+    game.play(1, 4);
+    game.loadView();
+};
--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,43 @@
 class Game {
-  constructor(player1, player2, boardSize) {
-    this.player1 = player1;
-    this.player2 = player2;
-    this.player1Holes = new Array(boardSize).fill(6);
-    this.player2Holes = new Array(boardSize).fill(6);
-    this.player1Storage = 0;
-    this.player2Storage = 0;
-  }
+    constructor(player1, player2, boardSize = 5, startSeeds = 6) {
+        this.players = [player1, player2];
+        this.board = [
+            new Array(boardSize).fill(startSeeds),
+            0,
+            new Array(boardSize).fill(startSeeds),
+            0,
+        ];
+        console.log(this.board);
+    }
+
+    play(player, position) {
+        if (position < 0 || position >= this.board[0].length) {
+            throw "Invalid position";
+        }
+        if (player !== 1 && player !== 0) {
+            throw "Invalid player";
+        }
+        const cof = player === 0 ? -1 : 1;
+        this.board[player * 2][position] = 0;
+    }
+
+    loadView() {
+        const rows = document.getElementsByClassName("hole-row");
+        for (let i = 0; i < this.board[0].length; i++) {
+            for (let rowNumber of [0, 1]) {
+                const hole = document.createElement("div");
+                hole.classList.add("hole");
+                rows[rowNumber].appendChild(hole);
+                let numberSeeds = this.board[rowNumber * 2][i];
+                while (numberSeeds--) {
+                    const seed = document.createElement("div");
+                    seed.classList.add("seed");
+                    seed.style.position = "relative";
+                    hole.appendChild(seed);
+                }
+            }
+        }
+    }
 }
 
 export default Game;
