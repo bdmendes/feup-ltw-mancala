f6d0e78fcb2f5bf7dd94875e7b2dae231c00bc26
Bruno Mendes <brod56@sapo.pt>
2021-12-22 12:01:30 +0000

    Add game logic and board updates

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import Game from "./scripts/game.js";
 import Player from "./scripts/player.js";
 
 window.onload = function () {
-    console.log("Hello world");
     const game = new Game(
         new Player("Jose", "sirze", "123"),
         new Player("Bruno", "brocolo", 345)
     );
-    console.log(game.players[0].name);
-    game.play(1, 4);
-    game.loadView();
+    console.log(JSON.parse(JSON.stringify(game.board)));
+    game.play_(1, 0);
+    game.play_(0, 0);
+    game.play_(1, 4);
 };
--- scripts/game.js
+++ scripts/game.js
@@ -1,27 +1,86 @@
 class Game {
-    constructor(player1, player2, boardSize = 5, startSeeds = 6) {
-        this.players = [player1, player2];
+    constructor(player1, player2, firstToPlay = 1, boardSize = 5, startSeeds = 6) {
+        this.player1 = player1;
+        this.player2 = player2;
         this.board = [
+            // Player 0 (top)
             new Array(boardSize).fill(startSeeds),
             0,
+
+            // Player 1 (bottom)
             new Array(boardSize).fill(startSeeds),
             0,
         ];
-        console.log(this.board);
+        this.currentToPlay = firstToPlay;
+        this.loadRowsView_();
     }
 
-    play(player, position) {
-        if (position < 0 || position >= this.board[0].length) {
-            throw "Invalid position";
+    play_(player, position) {
+        let remainingSeeds = this.board[player * 2][position];
+        if (remainingSeeds === 0) return;
+
+        let cof = player === 0 ? -1 : 1;
+        const originalCof = cof;
+        const playerSide = (currCof) => currCof === originalCof;
+        this.removeSeeds_(player, position);
+
+        while (remainingSeeds) {
+            position += cof;
+            if (position === -1 || position === this.board[0].length) {
+                if (playerSide(cof)) {
+                    remainingSeeds--;
+                    this.addSeed_(player, position);
+                }
+                cof *= -1;
+            } else {
+                remainingSeeds--;
+                this.addSeed_(cof === -1 ? 0 : 1, position);
+                if (this.board[player * 2][position] === 1 && playerSide(cof)) {
+                    let seedsStorage = this.board[0][position] + this.board[2][position];
+                    while (seedsStorage--)
+                        this.addSeed_(_, player === 0 ? -1 : this.board[0].length);
+                    this.removeSeeds_(0, position);
+                    this.removeSeeds_(1, position);
+                }
+            }
+            // console.log(JSON.parse(JSON.stringify(this.board)));
         }
-        if (player !== 1 && player !== 0) {
-            throw "Invalid player";
-        }
-        const cof = player === 0 ? -1 : 1;
-        this.board[player * 2][position] = 0;
+        if (position === -1 && player === 0) return;
+        if (position === this.board[0].length && player === 1) return;
+        this.currentToPlay = (this.currentToPlay + 1) % 2;
     }
 
-    loadView() {
+    getHoleNode_(player, position) {
+        const storages = document.getElementsByClassName("storage");
+        const holeRow = document.getElementsByClassName("hole-row")[player];
+        if (position === -1) {
+            return storages[0].getElementsByClassName("hole")[0];
+        } else if (position === this.board[0].length) {
+            return storages[1].getElementsByClassName("hole")[0];
+        } else {
+            return holeRow.getElementsByClassName("hole")[position];
+        }
+    }
+
+    addSeed_(player, position) {
+        if (position === -1) this.board[1]++;
+        else if (position === this.board[0].length) this.board[3]++;
+        else this.board[player * 2][position]++;
+
+        const seed = document.createElement("div");
+        seed.classList.add("seed");
+        this.getHoleNode_(player, position).appendChild(seed);
+    }
+
+    removeSeeds_(player, position) {
+        if (position === -1) this.board[1] = 0;
+        else if (position === this.board[0].length) this.board[3] = 0;
+        else this.board[player * 2][position] = 0;
+
+        this.getHoleNode_(player, position).innerHTML = "";
+    }
+
+    loadRowsView_() {
         const rows = document.getElementsByClassName("hole-row");
         for (let i = 0; i < this.board[0].length; i++) {
             for (let rowNumber of [0, 1]) {
@@ -29,11 +88,9 @@ class Game {
                 hole.classList.add("hole");
                 rows[rowNumber].appendChild(hole);
                 let numberSeeds = this.board[rowNumber * 2][i];
+                this.board[rowNumber * 2][i] = 0;
                 while (numberSeeds--) {
-                    const seed = document.createElement("div");
-                    seed.classList.add("seed");
-                    seed.style.position = "relative";
-                    hole.appendChild(seed);
+                    this.addSeed_(rowNumber, i);
                 }
             }
         }
