7164716527b9729096a7987dd5772feeffcd7b37
up201907216 <up201907216@edu.fe.up.pt>
2022-01-06 14:11:16 +0000

    Add interface scripts

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import Game from "./scripts/game.js";
 import Player from "./scripts/player.js";
 
 window.onload = function () {
-    const game = new Game();
-    game.board = [
-        [0, 1, 0, 2],
-        [1, 0, 1, 1],
-    ];
-    game.storage = [3, 4];
-    game.loadView();
-    game.calculateBestPlay(2);
+  const game = new Game();
+  game.board = [
+    [0, 1, 0, 2],
+    [1, 0, 1, 1],
+  ];
+  game.storage = [3, 4];
+  game.loadView();
+  game.calculateBestPlay(2);
 };
