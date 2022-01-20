427b8ffbc5d42f515c56c1e9226b05e6f2e5cd1f
Bruno Mendes <brod56@sapo.pt>
2022-01-20 05:02:48 +0000

    Add boilerplate server

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const http = require("http");
+const url = require("url");
+const { serveRanking } = require("./router/ranking");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!\n");
+function parseJson(request, response, callback) {
+    let body = "";
+    request.setEncoding("utf8");
+    request.on("data", (chunk) => {
+        body += chunk;
+    });
+    request.on("end", () => {
+        try {
+            const data = JSON.parse(body);
+            callback(data, response);
+        } catch (er) {
+            console.log("Invalid received json at route " + request.url);
+        }
+    });
+}
+
+http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true, false);
+    switch (request.method) {
+        case "POST":
+            switch (parsedUrl.pathname) {
+                case "/register":
+                    break;
+                case "/ranking":
+                    parseJson(request, response, serveRanking);
+                    break;
+                default:
+                    response.write("Unknown route");
+                    response.end();
+            }
+            break;
+        case "GET":
+            break;
+        default:
+            response.write("Unsupported HTTP method\n");
+            response.end();
+            break;
+    }
 }).listen(9109);
---
+++ router/ranking.js
@@ -0,0 +1,4 @@
+exports.serveRanking = (json, response) => {
+    response.write("json porreiro, senhor\n");
+    response.end();
+};
