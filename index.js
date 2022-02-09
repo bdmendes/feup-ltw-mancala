b44214c904893401b71829f18f2c98861fec7d8e
Bruno Mendes <brod56@sapo.pt>
2022-02-09 12:01:24 +0000

    Remove server code

--- index.js
+++ index.js
@@ -1,105 +1,61 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
-const crypto = require("crypto");
-const path = require("path");
-const { serveRanking } = require("./router/ranking");
-const { register } = require("./router/register");
+import Game from "./scripts/game.js";
+import { RemoteRoom, ComputerRoom } from "./scripts/room.js";
+import { LocalPlayer, RemotePlayer } from "./scripts/player.js";
 
-const mimeTypes = {
-    ".html": "text/html",
-    ".js": "text/javascript",
-    ".css": "text/css",
-    ".json": "application/json",
-    ".png": "image/png",
-    ".jpg": "image/jpg",
-    ".gif": "image/gif",
-    ".svg": "image/svg+xml",
-    ".wav": "audio/wav",
-    ".mp4": "video/mp4",
-    ".woff": "application/font-woff",
-    ".ttf": "application/font-ttf",
-    ".eot": "application/vnd.ms-fontobject",
-    ".otf": "application/font-otf",
-    ".wasm": "application/wasm",
+function clearHoles() {
+    for (let row of document.getElementsByClassName("hole-row")) {
+        while (row.firstChild) {
+            row.firstChild.remove();
+        }
+    }
+}
+
+function setupGame(form) {
+    /* Parse form */
+    const mode = form.mode.value;
+    let difficulty, turn, code, username, password, cavities, seeds;
+    if (mode === "single_player") {
+        difficulty = parseInt(form.difficulty.value);
+        turn = parseInt(form.user_turn.value);
+        if (turn === -1) {
+            turn = Math.floor(Math.random() * 2);
+        }
+    } else {
+        code = form.code.value;
+        username = form.username.value;
+        password = form.password.value;
+    }
+    cavities = parseInt(form.cavities.value);
+    seeds = parseInt(form.seeds.value);
+
+    /* Create entities */
+    if (mode == "single_player") {
+        window.room = new ComputerRoom(new Game(turn, cavities, seeds), new LocalPlayer("guest", ""), difficulty);
+        window.room.enterGameView();
+    } else {
+        window.room = new RemoteRoom(new Game(0, cavities, seeds), new RemotePlayer(username, password), code);
+    }
+}
+
+window.onload = function () {
+    document.getElementById("game_button").addEventListener("click", () => {
+        showPopup("Start Game");
+        document.getElementById("play").addEventListener("click", () => {
+            if (window.room != null && window.room.ready) {
+                if (
+                    !window.confirm(
+                        "Current game progress will be lost. Are you sure you want to leave and create a new game?"
+                    )
+                ) {
+                    return;
+                }
+                window.room.leave();
+            }
+            clearHoles();
+            setupGame(document.forms[0]);
+        });
+    });
 };
-
-function processJson(request, response, callback) {
-    let body = "";
-    request.setEncoding("utf8");
-    request.on("data", (chunk) => {
-        body += chunk;
-    });
-    request.on("end", () => {
-        try {
-            const requestData = JSON.parse(body);
-            const responseData = callback(requestData, response, players);
-            response.end(JSON.stringify(responseData), "utf-8");
-        } catch (_) {
-            response.writeHead(400);
-            response.end(
-                JSON.stringify({
-                    error: "Invalid request data",
-                })
-            );
-        }
-    });
-}
-
-function serveFile(pathName, response) {
-    console.log("serving " + pathName);
-    if (pathName === "/") {
-        pathName += "index.html";
-    }
-    const extName = String(path.extname(pathName)).toLowerCase();
-    const contentType = mimeTypes[extName] || "application/octet-stream";
-    fs.readFile("./public" + pathName, (error, content) => {
-        if (error) {
-            response.writeHead(404);
-            response.end(JSON.stringify(error));
-            return;
-        }
-        response.writeHead(200, { "Content-Type": contentType });
-        response.end(content, "utf-8");
-    });
-}
-
-let players = JSON.parse(fs.readFileSync("./local/players.json"));
-
-http.createServer((request, response) => {
-    const parsedUrl = url.parse(request.url, true, false);
-    switch (request.method) {
-        case "POST":
-            switch (parsedUrl.pathname) {
-                case "/register":
-                    processJson(request, response, register);
-                    break;
-                case "/ranking":
-                    processJson(request, response, serveRanking);
-                    break;
-                default:
-                    response.writeHead(404, { "Content-Type": "application/json" });
-                    response.end(
-                        JSON.stringify({
-                            error: "Unknown route",
-                        })
-                    );
-            }
-            break;
-        case "GET":
-            switch (parsedUrl.pathname) {
-                default:
-                    serveFile(parsedUrl.pathname, response);
-                    break;
-            }
-            break;
-        default:
-            response.writeHead(404, { "Content-Type": "application/json" });
-            response.end(
-                JSON.stringify({
-                    error: "Unsupported method",
-                })
-            );
-            break;
-    }
-}).listen(9109);
+window.onbeforeunload = function () {
+    return "Do you really want to exit the app? Current game progress will be lost.";
+};
--- router/ranking.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-
-exports.serveRanking = (request, response) => {
-   response.writeHead(200, { "Content-Type": "application/json" });
-   return JSON.parse(fs.readFileSync("./local/ranking.json"));
-}
